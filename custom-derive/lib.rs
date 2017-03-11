bee7c59c6622268f87c1a474830d0b407b44f9d3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-12 01:00:44 +0900

    refactor1

--- custom-derive/lib.rs
+++ custom-derive/lib.rs
@@ -8,64 +8,121 @@ use syn::{MetaItem, NestedMetaItem, Lit};
 
 #[proc_macro_derive(Subcommand, attributes(clap))]
 pub fn subcommand(input: TokenStream) -> TokenStream {
-  let ast = syn::parse_derive_input(&input.to_string()).unwrap();
-  let gen = impl_subcommand(&ast);
-  gen.parse().unwrap()
+  let subcommand = syn::parse_derive_input(&input.to_string())
+    .and_then(Subcommand::new)
+    .unwrap();
+
+  subcommand.to_derived_tokens()
+    .parse()
+    .unwrap()
 }
 
-fn impl_subcommand(ast: &syn::DeriveInput) -> quote::Tokens {
-  let mut name = None;
-  let mut about = None;
+struct SubcommandAttribute {
+  name: quote::Tokens,
+  about: quote::Tokens,
+}
 
-  for attr in ast.attrs.iter().filter(|attr| attr.name() == "clap") {
-    let items = match attr.value {
-      MetaItem::List(_, ref items) => items,
-      _ => continue,
-    };
+impl SubcommandAttribute {
+  fn new(attrs: Vec<syn::Attribute>) -> Result<SubcommandAttribute, String> {
+    let mut name = None;
+    let mut about = None;
 
-    for item in items {
-      let (ident, value) = match *item { 
-        NestedMetaItem::MetaItem(MetaItem::NameValue(ref ident, Lit::Str(ref value, _))) => {
-          (ident, value)
-        }
-        _ => unreachable!(),
+    for attr in attrs.into_iter().filter(|attr| attr.name() == "clap") {
+      let items = match attr.value {
+        MetaItem::List(_, ref items) => items,
+        _ => continue,
       };
 
-      match ident.as_ref() {
-        "name" => name = Some(value.to_owned()),
-        "about" => about = Some(value.to_owned()),
-        _ => unreachable!(),
+      for item in items {
+        let (ident, value) = match *item { 
+          NestedMetaItem::MetaItem(MetaItem::NameValue(ref ident, Lit::Str(ref value, _))) => {
+            (ident, value)
+          }
+          _ => return Err("invalud attribute".into()),
+        };
+
+        match ident.as_ref() {
+          "name" => name = Some(value.to_owned()),
+          "about" => about = Some(value.to_owned()),
+          _ => return Err("invalid attribute name".into()),
+        }
       }
+    }
+
+    let name = name.map(|s| quote!(#s)).unwrap_or(quote!(env!("CARGO_PKG_NAME")));
+    let about = about.map(|s| quote!(#s)).unwrap_or(quote!(env!("CARGO_PKG_DESCRIPTION")));
+
+    Ok(SubcommandAttribute {
+      name: name,
+      about: about,
+    })
+  }
+}
+
+struct Subcommand {
+  ident: syn::Ident,
+  attr: SubcommandAttribute,
+  variants: Vec<syn::Variant>,
+}
+
+impl Subcommand {
+  fn new(ast: syn::DeriveInput) -> Result<Subcommand, String> {
+    let ident = ast.ident;
+    let attr = SubcommandAttribute::new(ast.attrs)?;
+
+    match ast.body {
+      syn::Body::Enum(variants) => {
+        Ok(Subcommand {
+          ident: ident,
+          attr: attr,
+          variants: variants,
+        })
+      }
+      syn::Body::Struct(_) => Err("#[derive(Subcommand)] is only supported for enum".into()),
     }
   }
 
-  let name = name.map(|s| quote!(#s)).unwrap_or(quote!(env!("CARGO_PKG_NAME")));
-  let about = about.map(|s| quote!(#s)).unwrap_or(quote!(env!("CARGO_PKG_DESCRIPTION")));
+  fn to_derived_tokens(&self) -> quote::Tokens {
+    let ident = &self.ident;
+    let name = &self.attr.name;
+    let about = &self.attr.about;
 
-  let ref variants = match ast.body {
-    syn::Body::Enum(ref variants) => variants,
-    syn::Body::Struct(_) => unreachable!(),
-  };
-
-  let ident = &ast.ident;
-  let subcommand_apps = impl_subcommand_app(variants);
-  let subcommand_froms = impl_subcommand_from(ident, variants);
-
-  quote! {
-    impl Subcommand for #ident {
-      fn app<'a, 'b: 'a>() -> clap::App<'a, 'b> {
-        clap::App::new(#name)
-          .about(#about)
-          .setting(clap::AppSettings::VersionlessSubcommands)
-          .setting(clap::AppSettings::SubcommandRequiredElseHelp)
-          #subcommand_apps
+    let subcommand_apps = {
+      join_tokens(self.variants.iter()
+        .map(|v| {
+          let (name, help) = get_attr_name_and_help(&v);
+          let help = help.unwrap_or("Put help message".to_owned());
+          quote! {
+        .subcommand(clap::SubCommand::with_name(#name).about(#help))
       }
-    }
-    impl<'a, 'b:'a> From<&'b clap::ArgMatches<'a>> for #ident {
-      fn from(m: &'b clap::ArgMatches<'a>) -> Self {
-        match m.subcommand() {
-          #subcommand_froms
-          _ => unreachable!(),
+        }))
+    };
+
+    let subcommand_froms = {
+      join_tokens(self.variants.iter()
+        .map(|v| {
+          let (name, _) = get_attr_name_and_help(&v);
+          let variant = &v.ident;
+          quote! { (#name, _) => #ident :: #variant(Default::default()), }
+        }))
+    };
+
+    quote! {
+      impl Subcommand for #ident {
+        fn app<'a, 'b: 'a>() -> clap::App<'a, 'b> {
+          clap::App::new(#name)
+            .about(#about)
+            .setting(clap::AppSettings::VersionlessSubcommands)
+            .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+            #subcommand_apps
+        }
+      }
+      impl<'a, 'b:'a> From<&'b clap::ArgMatches<'a>> for #ident {
+        fn from(m: &'b clap::ArgMatches<'a>) -> Self {
+          match m.subcommand() {
+            #subcommand_froms
+            _ => unreachable!(),
+          }
         }
       }
     }
@@ -101,26 +158,6 @@ fn get_attr_name_and_help(v: &syn::Variant) -> (String, Option<String>) {
   let name = name.unwrap_or_else(|| v.ident.as_ref().to_lowercase());
 
   (name, help)
-}
-
-fn impl_subcommand_app(variants: &[syn::Variant]) -> quote::Tokens {
-  join_tokens(variants.into_iter()
-    .map(|v| {
-      let (name, help) = get_attr_name_and_help(&v);
-      let help = help.unwrap_or("Put help message".to_owned());
-      quote! {
-        .subcommand(clap::SubCommand::with_name(#name).about(#help))
-      }
-    }))
-}
-
-fn impl_subcommand_from(enumname: &syn::Ident, variants: &[syn::Variant]) -> quote::Tokens {
-  join_tokens(variants.into_iter()
-    .map(|v| {
-      let (name, _) = get_attr_name_and_help(&v);
-      let ident = &v.ident;
-      quote! { (#name, _) => #enumname :: #ident(Default::default()), }
-    }))
 }
 
 fn join_tokens<I, T>(iter: I) -> quote::Tokens
